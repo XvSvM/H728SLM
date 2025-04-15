2a17353572b01e159df2d70cb748a44beb2ca0fb
XvSvM <vadiagadia@gmail.com>
2025-04-15 17:52:25 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_adt3-user
-add_lunch_combo twrp_adt3-userdebug
-add_lunch_combo twrp_adt3-eng
+twrp_adt3-user
+twrp_adt3-userdebug
+twrp_adt3-eng

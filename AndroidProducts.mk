732b51b77bb0dc07ba57d2cc547d9399ae2528d5
XvSvM <vadiagadia@gmail.com>
2025-04-18 12:53:56 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/adt3.mk
+    $(LOCAL_DIR)/twrp_adt3.mk
 
 COMMON_LUNCH_CHOICES := \
-    adt3-user \
-    adt3-userdebug \
-    adt3-eng
+    twrp_adt3-user \
+    twrp_adt3-userdebug \
+    twrp_adt3-eng

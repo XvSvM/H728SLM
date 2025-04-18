4dc69fd66e1fcde9d456a48660d50822498ba9f0
XvSvM <vadiagadia@gmail.com>
2025-04-18 15:50:42 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,190 +1,171 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 DEVICE_PATH := device/askey/adt3
 
-# For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
-
-# A/B
 AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    system_dlkm \
-    system \
-    vendor_dlkm \
-    vendor \
-    product
+
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_PREBUILT_ELF_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := 
+TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := generic
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := ASKEY-ADT3
-
-# Board Boot Header
-# Switch to version 3 since A14 update, moving recovery resources to vendor_boot
-BOARD_BOOT_HEADER_VERSION := 3
-
-# Board Default Values
-# Switch to --vendor_cmdline since header v3
-BOARD_VENDOR_CMDLINE := loop.max_part=4, mmcblk.perdev_minors=16, firmware_class.path=/vendor/etc/firmware, bootconfig
-# From 2048, switch to 4096 since header v3
-BOARD_PAGE_SIZE := 4096
-BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x03388000
-BOARD_TAGS_OFFSET := 0xfff88100
-BOARD_DTB_OFFSET := 0x03288000
-#BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(BOARD_VENDOR_CMDLINE)
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE) --board ""
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-#BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-
-# Board - type/size ?
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_USES_SYSTEM_DLKMIMAGE := true
+# Platform
+TARGET_BOARD_PLATFORM := diana
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := exdroid
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
-# Build Rules
-BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
-# Crypto
-TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
-TW_INCLUDE_FBE_METADATA_DECRYPT := true
-TW_PREPARE_DATA_MEDIA_EARLY := true
-
-# Debug
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-
-# Display
-TARGET_SCREEN_DENSITY := 240
-
-# DTB - prebuilt
+# Prebuilt
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-
-# Kernel
-TARGET_NO_KERNEL := true
-# Just defining it, to avoid unncessary error while building...
-BOARD_KERNEL_IMAGE_NAME := Image
-
-# Kernel - source
-TARGET_KERNEL_CONFIG := adt3_defconfig
-TARGET_KERNEL_SOURCE := kernel/askey/adt3
-
-# Metadata
-BOARD_USES_METADATA_PARTITION := true
+TARGET_RECOVERY_DEVICE_DIRS += $(BASE_DEVICE_PATH)
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
-# Define vendor_boot image size, as we will we moving recovery resource to it
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := $(BOARD_BOOTIMAGE_PARTITION_SIZE)
-# TODO: Fix hardcoded value
+BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_SUPER_PARTITION_SIZE := 9126805504
+
+# Dynamic Partition
 BOARD_SUPER_PARTITION_GROUPS := askey_dynamic_partitions
 BOARD_ASKEY_DYNAMIC_PARTITIONS_PARTITION_LIST := system system system_dlkm vendor vendor vendor_dlkm product product
-# TODO: Fix hardcoded value
-BOARD_ASKEY_DYNAMIC_PARTITIONS_SIZE := 9122611200
+BOARD_ASKEY_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
+BOARD_MAIN_PARTITION_LIST := \
+    system_dlkm \
+    system \
+    vendor_dlkm \
+    vendor \
+    product
 
-# Partitions - file type
+# File System
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := erofs
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := erofs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := erofs
-BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
-
-# Platform
-TARGET_BOARD_PLATFORM := diana
-
-# Recovery
-TARGET_NO_RECOVERY := true
-TARGET_RECOVERY_PIXEL_FORMAT := ARGB_8888
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
-TARGET_USES_MKE2FS := true
 
-# Recovery - Ramdisk
-# Since moving recovery resources to vendor_boot, ramdisk compression type became lz4-l from gzip
-BOARD_RAMDISK_USE_LZ4 := true
+BOARD_USES_VENDOR_DLKMIMAGE := true
 
-# Recovery - sdcard ?
-RECOVERY_SDCARD_ON_DATA := true
-
-# SAR
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-BOARD_SUPPRESS_SECURE_ERASE := true
-
-# SPL
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 99.87.36
-PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
-
-# TWRP Configuration
-# TWRP Configuration
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := false
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
-TW_DEVICE_VERSION := X96Qproplus
-TW_NO_SCREEN_TIMEOUT := true
-TW_EXCLUDE_TZDATA := true
-TW_EXCLUDE_NANO := true
-TW_EXCLUDE_BASH := true
-TW_EXCLUDE_PYTHON := true
-
-# Vendor Boot
-# Making sure recovery build don't have kernel in it--well, no kernel at all in tree anyway... LOL!
-BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
-# We are now moving to vendor_boot, hmmm, wait, really?
-BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-
-# Vendor Modules
-TW_LOAD_VENDOR_MODULES := true
-TW_LOAD_VENDOR_BOOT_MODULES := true
-TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
-
-# Workaround for copy_out error
+# Workaround for error copying vendor files to recovery ramdisk
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_SYSTEM_DLKM := system_dlkm
 TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
+
+# Recovery
+TARGET_NO_RECOVERY := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+
+
+# Image settings
+TARGET_NO_KERNEL := true
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_ARCH := $(TARGET_ARCH)
+TARGET_KERNEL_HEADER_ARCH := $(TARGET_ARCH)
+BOARD_PAGE_SIZE := 2048
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_KERNEL_CMDLINE := loop.max_part=4 mmcblk.perdev_minors=16 firmware_class.path=/vendor/etc/firmware bootconfig
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_RAMDISK_OFFSET := 0x03388000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_TAGS_OFFSET := 0xfff88100
+BOARD_DTB_SIZE := 176221
+BOARD_DTB_OFFSET := 0x03288000
+BOARD_HEADER_SIZE := 2128
+
+# Vendor CMD
+BOARD_VENDOR_CMDLINE :=  loop.max_part=4 mmcblk.perdev_minors=16 firmware_class.path=/vendor/etc/firmware bootconfig
+
+# MkBootImage Args
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE) --board ""
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+BOARD_MKBOOTIMG_ARGS += --vendor_cmdline $(BOARD_VENDOR_CMDLINE)
+BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
+
+# Android Verified Boot
+BOARD_AVB_ENABLE := true
+
+# TWRP Configuration
+TW_DEFAULT_LANGUAGE := en
+TW_EXTRA_LANGUAGES := false
+TW_USE_TOOLBOX := true
+TARGET_USES_MKE2FS := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_NO_FASTBOOT_BOOT := true
+TW_INCLUDE_REPACKTOOLS := true
+
+# TWRP Display
+TW_DEFAULT_BRIGHTNESS := 400
+TW_MAX_BRIGHTNESS := 2047
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_FRAMERATE := 60
+TW_THEME := portrait_hdpi
+
+# TWRP Modules
+TW_INCLUDE_LPDUMP := true
+TW_INCLUDE_LPTOOLS := true
+TW_INCLUDE_LIBRESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_EXCLUDE_APEX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_NANO := true
+TW_EXCLUDE_PYTHON := true
+
+# TWRP Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+BOARD_USES_METADATA_PARTITION := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_FSCRYPT_POLICY := 2
+TW_FORCE_KEYMASTER_VER := true
+
+
+# TWRP Backup options
+TW_BACKUP_EXCLUSIONS := /data/fonts/files
+
+# TWRP Debug-tools
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+
+# TWRP Battery
+TW_USE_LEGACY_BATTERY_SERVICES := true
+
+# Maintainer/Version
+TW_DEVICE_VERSION := X96QPROPLUS
+
+
+# Vendor_boot recovery ramdisk
+BOARD_USES_RECOVERY_AS_BOOT :=
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT :=
+BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE :=
+BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT :=
+
+BOARD_VENDOR_RAMDISK_FRAGMENTS := recovery
+BOARD_VENDOR_RAMDISK_FRAGMENT.recovery.MKBOOTIMG_ARGS := --ramdisk_type RECOVERY
+

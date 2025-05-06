68fb7a7521624498168db059ba5c6c5617458e4d
XvSvM <vadiagadia@gmail.com>
2025-05-06 14:21:20 +0300

    Update twrp_adt3.mk

--- twrp_adt3.mk
+++ twrp_adt3.mk
@@ -6,14 +6,34 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Twrp stuff.
+# Configure core_64_bit_only.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Configure virtual_ab compression.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Configure twrp common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from adt3 device
 $(call inherit-product, device/askey/adt3/device.mk)
+
+# SHIPPING API
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# VNDK API
+PRODUCT_TARGET_VNDK_VERSION := 34
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 PRODUCT_DEVICE := adt3
 PRODUCT_NAME := twrp_adt3

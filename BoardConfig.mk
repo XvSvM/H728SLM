4f5c10a4c822e7685150d08a1c34724fa80c97c8
XvSvM <vadiagadia@gmail.com>
2025-05-06 11:14:30 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := diana
 
 # Kernel
+BOARD_KERNEL_CMDLINE := loop.max_part=4 mmcblk.perdev_minors=16 firmware_class.path=/vendor/etc/firmware bootconfig
 TARGET_KERNEL_ARCH            := arm64
 TARGET_KERNEL_HEADER_ARCH     := arm64
 BOARD_KERNEL_IMAGE_NAME       := Image

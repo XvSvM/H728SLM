8f12e5023be5af68c282a18a8abdf90834912b21
XvSvM <vadiagadia@gmail.com>
2025-05-14 14:51:04 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,6 +6,13 @@
 #
 
 LOCAL_PATH := device/askey/adt3
+
+# vendor_boot as recovery
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
